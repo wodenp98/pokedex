d3a6e7b0b111df11340d717ec38c31975e8d2936
wodenp98 <paulvigneron4698@gmail.com>
2024-01-29 15:53:38 +0100

    move to tailwind to pandacss

---
+++ panda.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "@pandacss/dev";
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // Where to look for your css declarations
+  include: ["./components/**/*.{ts,tsx,js,jsx}", "./app/**/*.{ts,tsx,js,jsx}"],
+
+  // Files to exclude
+  exclude: [],
+
+  // The output directory for your css system
+  outdir: "styled-system",
+});
