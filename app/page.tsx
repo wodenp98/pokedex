7ed38e5facadc2839ccdc5b78710330b9062294e
wodenp98 <paulvigneron4698@gmail.com>
2024-02-20 14:56:46 +0100

    add images for pokemons

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,55 @@
+import { Stack } from "~/styled-system/jsx";
+import * as Card from "~/components/ui/card";
 import { css } from "../styled-system/css";
 import { Button } from "~/components/ui/button";
+import Image from "next/image";
 
 async function getPokemons() {
   const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=30");
   const data = await res.json();
-  return data.results;
-}
-async function getOnePokemon(name: string) {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-  const data = await res.json();
-  return data;
+
+  // Récupérer les détails de chaque Pokémon
+  const pokemonDetails = await Promise.all(
+    data.results.map(async (pokemon: any) => {
+      const pokemonRes = await fetch(pokemon.url);
+      return await pokemonRes.json();
+    })
+  );
+
+  // Ajouter les détails aux objets Pokémon
+  const pokemonsWithDetails = data.results.map(
+    (pokemon: any, index: number) => {
+      return {
+        ...pokemon,
+        details: pokemonDetails[index],
+      };
+    }
+  );
+
+  return pokemonsWithDetails;
 }
 
 export default async function Home() {
   const pokemons = await getPokemons();
-  const pokemon = await getOnePokemon("bulbasaur");
-  console.log("pokemons", pokemons);
-  // console.log(pokemon.sprites.other["official-artwork"].front_default);
 
   return (
     <main>
-      <Button>Test</Button>
-      <div className={css({ mt: "4", fontSize: "xl", fontWeight: "semibold" })}>
-        John Doe
-      </div>
-      {/* {pokemons.map((pokemon: any) => (
-        <div key={pokemon.name}>
-          <h1>{pokemon.name}</h1>
-        </div>
-      ))} */}
+      {pokemons.map((pokemon: any) => (
+        <Card.Root width="sm" key={pokemon.name}>
+          <Card.Body>
+            <h1>{pokemon.name}</h1>
+            <Image
+              src={
+                pokemon.details.sprites.other?.["official-artwork"]
+                  ?.front_default ?? pokemon.details.sprites.front_default
+              }
+              alt={pokemon.name}
+              width={200}
+              height={200}
+            />
+          </Card.Body>
+        </Card.Root>
+      ))}
     </main>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    domains: ["raw.githubusercontent.com"],
+  },
 };
 
 module.exports = nextConfig;
