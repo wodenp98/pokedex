f56827baebfb9643e570e6c8453fe5883e496d9f
wodenp98 <paulvigneron4698@gmail.com>
2024-02-21 16:37:19 +0100

    styled pokemon's name

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import { Stack } from "~/styled-system/jsx";
 import * as Card from "~/components/ui/card";
-import { css, cx } from "../styled-system/css";
-import { Button } from "~/components/ui/button";
 import Image from "next/image";
-import { center, flex, grid } from "~/styled-system/patterns";
+import { center, grid } from "~/styled-system/patterns";
 
 async function getPokemons() {
   const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=30");
@@ -35,38 +32,38 @@ export default async function Home() {
 
   return (
     <main>
-      <div className={center({ h: "full" })}>
-        <div
-          className={css({
-            display: "flex",
-            flexDirection: "column",
-            fontWeight: "semibold",
-            color: "yellow.300",
-            textAlign: "center",
-            textStyle: "4xl",
-          })}
-        >
-          <span>🐼</span>
-          <span>Hello from Panda</span>
+      <div className={center({ h: "full", my: "4" })}>
+        <div className={grid({ gap: 4, columns: 3 })}>
+          {pokemons.map((pokemon: any) => (
+            <Card.Root
+              width="sm"
+              key={pokemon.name}
+              className={center({ h: "full" })}
+            >
+              <Card.Body>
+                <Image
+                  src={
+                    pokemon.details.sprites.other?.["official-artwork"]
+                      ?.front_default ?? pokemon.details.sprites.front_default
+                  }
+                  alt={pokemon.name}
+                  width={200}
+                  height={200}
+                />
+                <h1
+                  className={center({
+                    my: "2",
+                    fontWeight: "bold",
+                    textStyle: "md",
+                    textTransform: "uppercase",
+                  })}
+                >
+                  {pokemon.name}
+                </h1>
+              </Card.Body>
+            </Card.Root>
+          ))}
         </div>
-      </div>
-      <div className={grid({ gap: 4, columns: 3 })}>
-        {pokemons.map((pokemon: any) => (
-          <Card.Root width="sm" key={pokemon.name}>
-            <Card.Body>
-              <h1>{pokemon.name}</h1>
-              <Image
-                src={
-                  pokemon.details.sprites.other?.["official-artwork"]
-                    ?.front_default ?? pokemon.details.sprites.front_default
-                }
-                alt={pokemon.name}
-                width={200}
-                height={200}
-              />
-            </Card.Body>
-          </Card.Root>
-        ))}
       </div>
     </main>
   );
